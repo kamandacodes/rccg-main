9409dd4fb956921162b7b3be1c79a80f225c70f6
kamandacodes <kamandacode@gmail.com>
2021-09-18 14:11:20 +0100

    homepage grid

---
+++ src/components/FourColorGrid.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="bg-white shadow-lg grid grid-cols-2  mb-6 rounded-lg">
+      <p class="p-8 mb-0 border-blue-900 bg-blue-500">
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Qui molestias at, amet dolor illo magni. Earum, assumenda possimus. Perferendis accusamus mollitia deleniti quia ipsum reprehenderit voluptas impedit iusto architecto? Magnam eaque nemo, esse tempore quis eum doloribus assumenda quos nulla reprehenderit ab obcaecati nostrum perspiciatis aliquid, at accusantium, enim ratione? Non, aliquam possimus enim impedit neque, consequatur modi sed, recusandae commodi doloremque maxime. Iusto illum ex voluptas magni quos eveniet earum fugiat, accusamus mollitia nihil magnam harum, dolorum doloribus corporis similique perspiciatis est enim facilis. Voluptas, ipsa nobis voluptatibus sint consectetur eius perspiciatis quia quas alias expedita amet harum quam.
+      </p>
+      <p class="p-8 mb-0 bg-orange-400">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde ducimus ullam sed perferendis illum ipsa a incidunt enim necessitatibus exercitationem, maiores labore rerum eius deserunt reprehenderit aut doloremque quo asperiores fugiat consequatur quidem sunt. Ipsam aut exercitationem eum minima possimus neque quam accusantium tempore architecto dignissimos ea, doloribus repellat ducimus incidunt sapiente, dolorum nobis quas sunt libero. Aspernatur suscipit doloremque, perspiciatis earum nulla amet cumque laboriosam, odit laborum eos delectus quos dolorem non? Suscipit possimus voluptatum ad debitis nihil assumenda doloribus incidunt perspiciatis! Corporis, exercitationem fuga animi nemo modi magni voluptates neque cum inventore, quaerat consequuntur dolore recusandae sint quia!
+      </p>
+      <p class="p-8 mb-0 bg-green-500">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta esse ut atque non vero dolor natus inventore harum cumque consequatur corrupti quasi explicabo molestias eius voluptatibus, asperiores modi in consequuntur architecto deserunt? Ducimus eos quidem totam cupiditate nemo deserunt, velit facilis similique placeat amet voluptates sed dolorum, quod suscipit! Blanditiis pariatur voluptas eligendi magnam quis omnis earum optio excepturi, dolor culpa dolorem, eveniet ex labore suscipit dolorum. Laudantium minus consectetur nisi commodi voluptates maiores alias, pariatur repellat qui hic magni quia. Repellat inventore consequuntur nisi asperiores, officiis, fugit necessitatibus expedita voluptatum commodi qui impedit facilis ipsa cum, eveniet tenetur est?
+      </p>
+      <p class="p-8 mb-0 bg-teal-400">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam sit ea tempore, adipisci eum, veniam reiciendis amet illo dolore inventore iusto quisquam ratione exercitationem nisi quam vitae ex dolorem corrupti. Amet facilis deleniti hic possimus ratione dignissimos, dolores unde officia odit quibusdam consequatur temporibus? Veritatis earum ullam velit blanditiis eaque optio sequi, tempore molestias? Cupiditate perferendis aliquam quam laboriosam corporis, saepe eveniet at sunt ullam dicta unde aut expedita vel illum sit magni ducimus impedit placeat, mollitia omnis eos aperiam. Dignissimos, dolores? Eum, ad repellendus. Adipisci, minima obcaecati sed modi dicta illum deserunt cum minus quia debitis explicabo nulla numquam?
+      </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'FourColorGrid',
+    props: [
+        'post'
+    ],
+}
+</script>
